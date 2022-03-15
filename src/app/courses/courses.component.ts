d8a590d932c9c197450d6b839641425f80e9b425
victor.geodata <victor.geodata@gmail.com>
2022-03-15 14:30:01 -0300

    class binding

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -10,6 +10,7 @@ export class CoursesComponent implements OnInit {
   title = "List of Courses";
   imageUrl = "https://picsum.photos/400/200";
   colSpan = 2;
+  isActive:boolean = true;
 
   constructor() { }
 
