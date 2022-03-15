96df3725c06eb89452baa1873ef211bd65518411
victor.geodata <victor.geodata@gmail.com>
2022-03-15 16:12:45 -0300

    creating custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -22,6 +22,9 @@ export class CoursesComponent implements OnInit {
     releaseDate: new Date(2016, 3, 1)
   }
 
+  text = `Lorem ipsum dolor sit amet. Et dicta exercitationem aut corporis incidunt non libero ipsa eos tenetur sint ut deserunt molestiae. Aut repellendus aperiam est explicabo consectetur in odio consectetur eos ullam maiores! Nam beatae repudiandae ea voluptatibus praesentium qui quaerat voluptatem sed dolorem veniam et corrupti reiciendis et similique velit quo modi nihil. Et quae ipsam ut saepe praesentium rem harum obcaecati. Et quam error est laborum voluptatum vel voluptatem corporis. Et mollitia tenetur sit quos dolor ea rerum consectetur cum similique quae in atque dolorum. In quia minima non omnis quaerat aut voluptatibus eligendi. Et eaque expedita est maxime iusto ea nemo itaque. Aut laudantium placeat  consequatur tenetur non perferendis magni qui porro reiciendis et beatae repellendus rem facere dolor.
+  `
+
   constructor() { }
 
   ngOnInit(): void {
